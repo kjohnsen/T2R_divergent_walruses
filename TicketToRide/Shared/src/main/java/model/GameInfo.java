4f859397e5e4bcc59c06edaf44d10f31de45ab66
Kyle Johnsen <johnsenkyle13@gmail.com>
2018-09-26 14:53:40 -0600

    A bunch of stuff

--- TicketToRide/Shared/src/main/java/model/GameInfo.java
+++ TicketToRide/Shared/src/main/java/model/GameInfo.java
@@ -1,4 +1,15 @@
-package model;
+package main.java.model;
+
+import java.util.ArrayList;
 
 public class GameInfo {
+    private GameID gameID;
+    private ArrayList<Player> players;
+
+    public GameInfo(GameID gameID, ArrayList<Player> players) {
+        this.gameID = gameID;
+        this.players = players;
+    }
+
+
 }
