170aa37681edb1bbfdbe67c5ee6e721042d6bd7b
Kyle Johnsen <johnsenkyle13@gmail.com>
2018-09-29 16:57:45 -0600

    Flesh out ClientModel

--- app/src/main/java/model/ClientModel.java
+++ app/src/main/java/model/ClientModel.java
@@ -2,11 +2,19 @@ package model;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
 
+import modelclasses.GameInfo;
 import modelclasses.User;
 
-public class ClientModel implements PropertyChangeListener {
+public class ClientModel extends Observable {
     private User currentUser;
+
+    private ArrayList<GameInfo> gameList;
+
+    private GameInfo currentGame;
 
     private static final ClientModel ourInstance = new ClientModel();
 
@@ -17,8 +25,30 @@ public class ClientModel implements PropertyChangeListener {
     private ClientModel() {
     }
 
-    @Override
-    public void propertyChange(PropertyChangeEvent evt) {
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+        this.notifyObservers();
+    }
 
+    public void setGameList(ArrayList<GameInfo> gameList) {
+        this.gameList = gameList;
+        this.notifyObservers();
+    }
+
+    public void setCurrentGame(GameInfo currentGame) {
+        this.currentGame = currentGame;
+        this.notifyObservers();
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public ArrayList<GameInfo> getGameList() {
+        return gameList;
+    }
+
+    public GameInfo getCurrentGame() {
+        return currentGame;
     }
 }
