95170042765450a239e5309dbb330ecda27f8499
Kyle Johnsen <johnsenkyle13@gmail.com>
2018-09-29 13:07:55 -0600

    Setup for CI

---
+++ Shared/src/test/java/data/TestCommandManager.java
@@ -0,0 +1,12 @@
+package data;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestCommandManager {
+    @Test
+    public void testTest() {
+        assertEquals(4, 2+2);
+        assertTrue(false);
+    }
+}
