ea95e78d13d3b6b8e1ba4d469804dd4d0c0fcab9
Jake Griffey <jakegriffey@gmail.com>
2018-09-28 20:34:01 -0600

    changed iclient so UIFacade and CommandFacade can both implement it

--- app/src/main/java/model/UIFacade.java
+++ app/src/main/java/model/UIFacade.java
@@ -1,40 +1,54 @@
 package model;
 
+import clientserver.ServerPoller;
+import clientserver.ServerProxy;
 import interfaces.iClient;
 import modelclasses.GameID;
 import modelclasses.GameInfo;
 import modelclasses.Player;
 import modelclasses.PlayerColor;
+import results.LoggedInResults;
 import results.Results;
 
 public class UIFacade implements iClient {
+
     @Override
-    public Results joinGame(Player player, GameID gameID) {
-        return null;
+    public void loginUser(String username, String password) {
+        LoggedInResults loggedInResults = ServerProxy.getInstance().loginUser(username, password);
+        if(loggedInResults != null) {
+            //execute the command
+        } else {
+            //show an error through the model?
+        }
     }
 
     @Override
-    public Results createGame(GameInfo gameInfo) {
-        return null;
+    public void registerUser(String username, String password) {
+        LoggedInResults loggedInResults = ServerProxy.getInstance().registerUser(username, password);
+        if(loggedInResults != null) {
+            //execute the command
+        } else {
+            //show an error through the model?
+        }
     }
 
     @Override
-    public Results startGame(GameID gameID) {
-        return null;
-    }
-
-    @Override
-    public Results loginUser(String authToken) {
+    public void joinGame(Player player, GameID gameID) {
 
     }
 
     @Override
-    public Results registerUser(String authToken) {
-        return null;
+    public void createGame(GameInfo gameInfo) {
+
     }
 
     @Override
-    public Results claimColor(Player player, PlayerColor pc) {
-        return null;
+    public void startGame(GameInfo gameInfo) {
+
+    }
+
+    @Override
+    public void claimColor(Player player, PlayerColor playerColor) {
+
     }
 }
