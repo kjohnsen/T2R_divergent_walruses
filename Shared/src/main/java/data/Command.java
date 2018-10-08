2d37cad6d30a71713d8d82b401dedb8cbd1c311d
Jake Griffey <jakegriffey@gmail.com>
2018-10-08 13:34:13 -0600

    fixed commands

--- Shared/src/main/java/data/Command.java
+++ Shared/src/main/java/data/Command.java
@@ -7,6 +7,7 @@ import results.Results;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.rmi.ServerError;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,10 +16,10 @@ public class Command {
     private String _className;
     private String _methodName;
     private String[] _paramTypes;
-    private Object[] _paramValues;
-    private Serializer serializer = new Serializer();
+    private String[] _paramValues;
+    //private Serializer serializer = new Serializer();
 
-    public Command(String className, String methodName, String[] paramTypes, Object[] paramValues) {
+    public Command(String className, String methodName, String[] paramTypes, String[] paramValues) {
         _className = className;
         _methodName = methodName;
         _paramTypes = paramTypes;
@@ -28,8 +29,9 @@ public class Command {
     //another simplified constructor
     public Command(String className, String methodName, List<Object> parameters){
         ArrayList<String> paramTypes = new ArrayList<>();
-        ArrayList<Object> _parameters = new ArrayList<>();
+        ArrayList<String> _parameters = new ArrayList<>();
 
+        Serializer serializer = new Serializer();
         for(Object object : parameters) {
             paramTypes.add(object.getClass().getName());
             String serializedObject = serializer.encode(object);
@@ -39,7 +41,7 @@ public class Command {
         _className = className;
         _methodName = methodName;
         _paramTypes = paramTypes.toArray(new String[0]);
-        _paramValues = _parameters.toArray();
+        _paramValues = _parameters.toArray(new String[0]);
     }
 
     public String get_className() {
@@ -66,11 +68,11 @@ public class Command {
         this._paramTypes = _paramTypes;
     }
 
-    public Object[] get_paramValues() {
+    public String[] get_paramValues() {
         return _paramValues;
     }
 
-    public void set_paramValues(Object[] _paramValues) {
+    public void set_paramValues(String[] _paramValues) {
         this._paramValues = _paramValues;
     }
 
@@ -92,7 +94,7 @@ public class Command {
             //this is a round-about way of using an array of strings for class types because
             //gson cannot serialize an array of type: Class.
             Class<?>[] paramTypesArray;
-            Object[] paramValues;
+            String[] paramValues;
 
             //turn param types from string to Class
             ArrayList<Class<?>> paramTypesList = new ArrayList<>();
@@ -106,13 +108,15 @@ public class Command {
             Method method = receiver.getMethod(get_methodName(), paramTypesArray);
 
 
+            Serializer serializer = new Serializer();
+            ArrayList<Object> realValues = new ArrayList<>();
             for (int i = 0; i < paramValues.length; i++) {
-                paramValues[i] = serializer.decode((String)paramValues[i], paramTypesArray[i]);
+                realValues.add(serializer.decode(paramValues[i], paramTypesArray[i]));
             }
 
             //Object target = getTargetInstance(receiver);
 
-            return (Results) method.invoke(null, paramValues);
+            return (Results) method.invoke(null, realValues.toArray());
         }
         catch (Exception e) {
             e.printStackTrace();
