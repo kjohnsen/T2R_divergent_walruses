b4737f39734fdcc324f7a0d221045021a2153722
Kyle Johnsen <johnsenkyle13@gmail.com>
2018-10-17 17:19:02 -0600

    Create City, Route
    (cherry picked from commit c1be8e7)

---
+++ app/src/main/java/model/MapSetup.java
@@ -0,0 +1,55 @@
+package model;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+
+import java.util.List;
+
+import modelclasses.City;
+import modelclasses.Route;
+
+public class MapSetup {
+
+    public static List<Route> getRoutes() {
+    }
+    public static LatLngBounds getBounds() {
+
+    }
+
+    private City atlanta = new City("Atlanta", 33.75, -84.39);
+    private City boston = new City("Boston", 42.32, -71.08);
+    private City calgary = new City("Calgary", 51.08, -114.08);
+    private City charleston = new City("Charleston", 38.35, -81.63);
+    private City chicago = new City("Chicago", 41.84, -87.69);
+    private City dallas = new City("Dallas", 33.92, -84.84);
+    private City denver = new City("Denver", 39.76, -104.88);
+    private City duluth = new City("Duluth", 46.78, -92.13);
+    private City elPaso = new City("El Paso", 31.85, -106.43);
+    private City helena = new City("Helena", 46.60, -112.02);
+    private City houston = new City("Houston", 29.79, -95.39);
+    private City kansasCity = new City("kansasCity", 39.12, -94.55);
+    private City lasVegas = new City("lasVegas", 33.75, -84.39);
+    private City littleRock = new City("Little Rock", 33.75, -84.39);
+    private City losAngeles = new City("Los Angeles", 33.75, -84.39);
+    private City miami = new City("Miami", 33.75, -84.39);
+    private City montreal = new City("Montreal", 33.75, -84.39);
+    private City nashville = new City("Nashville", 33.75, -84.39);
+    private City newOrleans = new City("New Orleans", 33.75, -84.39);
+    private City newYork = new City("New York", 33.75, -84.39);
+    private City okCity = new City("Oklahoma City", 33.75, -84.39);
+    private City omaha = new City("Omaha", 33.75, -84.39);
+    private City phoenix = new City("Phoenix", 33.75, -84.39);
+    private City pittsburgh = new City("Pittsburgh", 33.75, -84.39);
+    private City portland = new City("Portland", 33.75, -84.39);
+    private City raleigh = new City("Raleigh", 33.75, -84.39);
+    private City saintLouis = new City("Saint Louis", 33.75, -84.39);
+    private City slc = new City("Salt Lake City", 33.75, -84.39);
+    private City sanFrancisco = new City("San Francisco", 33.75, -84.39);
+    private City santaFe = new City("Santa Fe", 33.75, -84.39);
+    private City saultStMarie= new City("Sault St. Marie", 33.75, -84.39);
+    private City seattle = new City("Seattle", 33.75, -84.39);
+    private City toronto = new City("Toronto", 33.75, -84.39);
+    private City vancouver = new City("Vancouver", 33.75, -84.39);
+    private City washington = new City("Washington", 33.75, -84.39);
+    private City winnipeg = new City("Winnipeg", 33.75, -84.39);
+}
