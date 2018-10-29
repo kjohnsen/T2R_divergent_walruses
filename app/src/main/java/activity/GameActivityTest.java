ff4ba2b0f201239119ec3f88efc4684068e76cfe
Kyle <johnsenkyle13@gmail.com>
2018-10-29 10:30:31 -0600

    Commit

--- app/src/main/java/activity/GameActivityTest.java
+++ app/src/main/java/activity/GameActivityTest.java
@@ -21,7 +21,7 @@ import model.ClientModel;
 import modelclasses.GameInfo;
 import modelclasses.Player;
 
-public class GameActivityTest extends AppCompatActivity {
+public class GameActivityTest extends GameActivity {
 
     ViewPager pager;
     MyPagerAdapter adapter;
@@ -45,43 +45,5 @@ public class GameActivityTest extends AppCompatActivity {
         setupTest();
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
-        pager = findViewById(R.id.pager);
-        tabs = findViewById(R.id.tabLayout);
-        adapter = new MyPagerAdapter(getSupportFragmentManager());
-        pager.setAdapter(adapter);
-        tabs.setupWithViewPager(pager);
-        tabs.setTabMode(TabLayout.MODE_SCROLLABLE);
-    }
-
-    public static class MyPagerAdapter extends FragmentPagerAdapter {
-        private static int NUM_ITEMS = 4;
-        public MyPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-        @Override
-        public int getCount() {
-            return NUM_ITEMS;
-        }
-        @Override
-        public Fragment getItem(int position) {
-            switch (position) {
-                case 0: return new PlayerInfoFragment();
-                case 1: return new GameInfoFragment();
-                case 2: return new DecksFragment();
-                case 3: return new ChatFragment();
-                default: return null;
-            }
-        }
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0: return "Player Info";
-                case 1: return "Game Info";
-                case 2: return "Decks";
-                case 3: return "Chat";
-                default: return null;
-            }
-        }
     }
 }
