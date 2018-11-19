407c7a5c9039f8ef6cbf667687407f16dddc12a1
Jake Griffey <jakegriffey@gmail.com>
2018-11-19 09:38:34 -0700

    updated game

--- app/src/main/java/model/CommandFacade.java
+++ app/src/main/java/model/CommandFacade.java
@@ -156,6 +156,7 @@ public class CommandFacade implements iClient {
 
     @Override
     public void startGame(GameInfo game) {
+        ClientModel.getInstance().setCurrentGame(game);
         ClientModel.getInstance().setFaceupCards(game.getFaceUpCards());
         ClientModel.getInstance().setCurrentGamePlayers(game.getPlayers());
 
